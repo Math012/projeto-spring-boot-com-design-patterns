5073534c518e86a3829552d84de968a0dfc2c06b
Math012 <math111.0811@gmail.com>
2025-02-11 18:47:35 -0300

    Adicionando a anotacao do FeignClient para consumir o endpoint do ViaCep

---
+++ src/main/java/com/math012/padroes_projeto_spring/service/ViaCepService.java
@@ -0,0 +1,14 @@
+package com.math012.padroes_projeto_spring.service;
+
+import com.math012.padroes_projeto_spring.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "viacep", url = " viacep.com.br/ws")
+public interface ViaCepService {
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
