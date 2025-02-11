b015d814278ceed5115246ee8ddecd21472ad5b3
Math012 <math111.0811@gmail.com>
2025-02-11 17:56:29 -0300

    Primeiro commit

---
+++ src/main/java/com/math012/padroes_projeto_spring/PadroesProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.math012.padroes_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/math012/padroes_projeto_spring/PadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.padroes_projeto_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
