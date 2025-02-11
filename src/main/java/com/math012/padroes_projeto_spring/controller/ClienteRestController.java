d42f5ecb379932e775e79c57a61bd548a6386c26
Math012 <math111.0811@gmail.com>
2025-02-11 18:49:10 -0300

    Criando o ClienteRestController e adicionando os metodos do ClienteServiceImpl

---
+++ src/main/java/com/math012/padroes_projeto_spring/controller/ClienteRestController.java
@@ -0,0 +1,44 @@
+package com.math012.padroes_projeto_spring.controller;
+
+import com.math012.padroes_projeto_spring.model.Cliente;
+import com.math012.padroes_projeto_spring.service.ClienteService;
+import feign.Client;
+import feign.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ClienteRestController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Cliente>> buscarTodos(){
+        return ResponseEntity.ok(clienteService.buscarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id){
+        return ResponseEntity.ok(clienteService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente){
+        clienteService.inserir(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> atualizar(@PathVariable("id")Long id, @RequestBody Cliente cliente){
+        clienteService.atualizar(id, cliente);
+        return ResponseEntity.ok("Cliente atualizado com sucesso: " + cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id")Long id){
+        clienteService.deletar(id);
+        return ResponseEntity.ok().build();
+    }
+}
