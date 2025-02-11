441ff9ce49dee7c9074ec4429ee3033cf0db02bb
Math012 <math111.0811@gmail.com>
2025-02-11 18:44:03 -0300

    Implementando a interface ClienteService

---
+++ src/main/java/com/math012/padroes_projeto_spring/service/ClienteServiceImpl.java
@@ -0,0 +1,30 @@
+package com.math012.padroes_projeto_spring.service;
+
+import com.math012.padroes_projeto_spring.model.Cliente;
+
+public class ClienteServiceImpl implements ClienteService{
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
