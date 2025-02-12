f1ae55c86dbc5d09a3ad315599d6c94ce0f25da0
Math012 <math111.0811@gmail.com>
2025-02-12 15:14:16 -0300

    Implementando a interface ExchangeService com a implementacao do metodo consultarCambio()

---
+++ src/main/java/com/math012/padroes_projeto_spring/service/exchange/ExchangeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.math012.padroes_projeto_spring.service.exchange;
+
+import com.math012.padroes_projeto_spring.model.ExchangeRate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class ExchangeServiceImpl implements ExchangeService{
+
+    @Autowired
+    private ExchangeRateService exchangeRateService;
+
+    @Override
+    public String consultarCambio(String de, String para) {
+        ExchangeRate exchangeRate = exchangeRateService.consultarCambio(de);
+        Map<String, Double> rates = exchangeRate.getRates().listaDeMoedasDisponiveis();
+        return "A moeda " + de + " convertida para a moeda: " + para + ", tem o valor de: " + rates.get(para);
+    }
+}
