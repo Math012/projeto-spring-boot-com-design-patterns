94215c21594008bde4c9ea198f8ad873851a0079
Math012 <math111.0811@gmail.com>
2025-02-12 15:43:30 -0300

    Implementando a classe CustomHandlerException com a excecao CambioException

--- src/main/java/com/math012/padroes_projeto_spring/exception/CustomHandlerException.java
+++ src/main/java/com/math012/padroes_projeto_spring/exception/CustomHandlerException.java
@@ -1,9 +1,20 @@
 package com.math012.padroes_projeto_spring.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class CustomHandlerException extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(CambioException.class)
+    public ResponseEntity<StructException> handlerCambioException(Exception e, WebRequest request){
+        var exception = new StructException(new Date(),e.getMessage(),request.getDescription(false));
+        return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+    }
 }
