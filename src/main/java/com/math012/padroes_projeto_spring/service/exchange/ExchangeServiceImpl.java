2db0ecd50d20839dde33dcbe93043c9b9d05a8c2
Math012 <math111.0811@gmail.com>
2025-02-12 15:47:56 -0300

    Implementando a excecao para validar se a moeda seleciona esta registrada na classe modelo

--- src/main/java/com/math012/padroes_projeto_spring/service/exchange/ExchangeServiceImpl.java
+++ src/main/java/com/math012/padroes_projeto_spring/service/exchange/ExchangeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.math012.padroes_projeto_spring.service.exchange;
 
+import com.math012.padroes_projeto_spring.exception.CambioException;
 import com.math012.padroes_projeto_spring.model.ExchangeRate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,13 @@ public class ExchangeServiceImpl implements ExchangeService{
 
     @Override
     public String consultarCambio(String de, String para) {
-        ExchangeRate exchangeRate = exchangeRateService.consultarCambio(de);
-        Map<String, Double> rates = exchangeRate.getRates().listaDeMoedasDisponiveis();
-        return "A moeda " + de + " convertida para a moeda: " + para + ", tem o valor de: " + rates.get(para)+"\n"+"data: "+exchangeRate.getTimeLastUpdateUtc();
+        try {
+            ExchangeRate exchangeRate = exchangeRateService.consultarCambio(de);
+
+            Map<String, Double> rates = exchangeRate.getRates().listaDeMoedasDisponiveis();
+            return "A moeda " + de + " convertida para a moeda: " + para + ", tem o valor de: " + rates.get(para)+"\n"+"data: "+exchangeRate.getTimeLastUpdateUtc();
+        }catch (Exception e){
+            throw new CambioException("Selecione as moedas disponíveis: MXN, EUR, BRL, BOB, CLP, CNY, COP, UYU, VEF, ARS, USD");
+        }
     }
 }
