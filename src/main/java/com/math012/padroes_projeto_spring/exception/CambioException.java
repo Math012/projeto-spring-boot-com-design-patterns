96511d2ed6e70728199261c76d1ff419e7b5f5f8
Math012 <math111.0811@gmail.com>
2025-02-12 15:38:59 -0300

    Implementando a excecao CambioException

---
+++ src/main/java/com/math012/padroes_projeto_spring/exception/CambioException.java
@@ -0,0 +1,8 @@
+package com.math012.padroes_projeto_spring.exception;
+
+public class CambioException extends RuntimeException{
+
+    public CambioException(String err){
+        super(err);
+    }
+}
